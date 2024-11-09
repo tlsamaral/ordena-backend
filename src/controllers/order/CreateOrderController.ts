0103c9923f9bbeb32f21ed2c882e3a0f3c0c5870
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-08 22:11:31 -0300

    feat: create order controlle

--- src/controllers/order/CreateOrderController.ts
+++ src/controllers/order/CreateOrderController.ts
@@ -3,9 +3,9 @@ import { CreateOrderService } from '../../services/order/CreateOrderService'
 
 class CreateOrderController {
   async handle(req: Request, res: Response) {
-    const { table, name } = req.body
+    const { table_id, name } = req.body
     const createOrderService = new CreateOrderService()
-    const order = await createOrderService.execute({ table, name })
+    const order = await createOrderService.execute({ table_id, name })
 
     return res.json(order)
   }
