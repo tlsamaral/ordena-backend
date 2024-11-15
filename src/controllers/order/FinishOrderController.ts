52fb8a59dd83bd43358ca6ad2f7ac0a970d7ce33
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-15 16:28:16 -0300

    finish order service

--- src/controllers/order/FinishOrderController.ts
+++ src/controllers/order/FinishOrderController.ts
@@ -4,7 +4,9 @@ import { FinishOrderService } from '../../services/order/FinishOrderService'
 class FinishOrderController {
   async handle(req: Request, res: Response) {
     const { order_id } = req.body
-    const finishOrderService = new FinishOrderService()
+    const socketio = req.app.get('socketio')
+
+    const finishOrderService = new FinishOrderService(socketio)
     const order = await finishOrderService.execute({ order_id })
 
     return res.json(order)
