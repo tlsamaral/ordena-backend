d0ac9479650fa0805411a962082170baa8433c90
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-15 17:05:46 -0300

    finish order servic

--- src/services/order/FinishOrderService.ts
+++ src/services/order/FinishOrderService.ts
@@ -1,11 +1,21 @@
+import type { Server } from 'socket.io'
 import prismaClient from '../../prisma'
+import type { CustomSocketServer } from '../types/socket'
 
 interface OrderRequest {
   order_id: string
 }
 
 class FinishOrderService {
+  private io: CustomSocketServer
+  private userSockets: Map<string, string> // Mapeamento de userId -> socketId
+
+  constructor(io: CustomSocketServer) {
+    this.io = io
+  }
+
   async execute({ order_id }: OrderRequest) {
+    // Atualiza o status do pedido no banco de dados
     const order = await prismaClient.order.update({
       where: {
         id: order_id,
@@ -13,7 +23,27 @@ class FinishOrderService {
       data: {
         status: true,
       },
+      select: {
+        created_at: true,
+        status: true,
+        table: true,
+        user_id: true,
+        name: true,
+      },
     })
+
+    // Obtém o socketId associado ao user_id do pedido
+    const socketId = this.io.userSockets.get(order.user_id)
+    if (socketId) {
+      console.log(
+        `Emitindo evento para user_id: ${order.user_id} com socketId: ${socketId}`,
+      )
+      // Emite o evento apenas para o socket específico do usuário
+      this.io.to(socketId).emit('order:finish', order)
+    } else {
+      console.log(`Usuário ${order.user_id} não está conectado no momento.`)
+    }
+
     return order
   }
 }
