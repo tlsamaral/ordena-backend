a09031df07d8f61f6efbf9b2c3e54e7877abe222
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-15 17:04:58 -0300

    create order service

--- src/services/order/CreateOrderService.ts
+++ src/services/order/CreateOrderService.ts
@@ -3,14 +3,16 @@ import prismaClient from '../../prisma'
 interface OrderRequest {
   table_id: string
   name: string
+  user_id: string
 }
 
 class CreateOrderService {
-  async execute({ table_id, name }: OrderRequest) {
+  async execute({ table_id, name, user_id }: OrderRequest) {
     const order = await prismaClient.order.create({
       data: {
         table_id,
         name,
+        user_id,
       },
       select: {
         id: true,
