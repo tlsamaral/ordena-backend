31eb7bf4ab41f24140d46878e5c5e9e3e5a95735
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-18 22:57:47 -0300

    create order

--- src/services/order/CreateOrderService.ts
+++ src/services/order/CreateOrderService.ts
@@ -1,27 +1,57 @@
 import prismaClient from '../../prisma'
+import { SendSms } from '../../utils/sendSms'
 
 interface OrderRequest {
   table_id: string
   name: string
   user_id: string
+  phone: string
 }
 
 class CreateOrderService {
-  async execute({ table_id, name, user_id }: OrderRequest) {
+  async execute({ table_id, name, user_id, phone }: OrderRequest) {
     const order = await prismaClient.order.create({
       data: {
         table_id,
         name,
         user_id,
+        status: 'A',
+        draft: true,
+        phone,
       },
-      select: {
-        id: true,
-        table: true,
-        name: true,
+      include: {
+        items: {
+          include: {
+            product: {
+              select: {
+                name: true,
+                banner: true,
+              },
+            },
+          },
+        },
+        table: {
+          select: {
+            name: true,
+          },
+        },
       },
     })
 
-    return order
+    const newOrder = {
+      ...order,
+      items: order.items.map((item) => ({
+        ...item,
+        product: {
+          ...item.product,
+          banner: item.product.banner
+            ? `${process.env.BASE_URL}/files/${item.product.banner}`
+            : null,
+        },
+      })),
+    }
+
+    return newOrder
   }
 }
 
